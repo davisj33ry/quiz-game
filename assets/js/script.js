b104ac5c4737a58546a67861efa84ec8bc2d8a99
dj3rry <davis.jerryjr@gmail.com>
2022-10-23 16:31:37 -0400

    updating html and styling

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question_1: "How many boroughs make up New York City?",
+        selection: ["One", "Three", "Five", "Seven"],
+        answer: "Five"
+    },
+    {
+        question_2: "Which river flows under the Boroklyn Bridge?",
+        selection: ["Hudson", "East River", "Mississippi", "Delaware"],
+        answer: "East River"
+    },
+    {
+        question_3: "Times Square is located in which borough?",
+        selection: ["Brooklyn", "Queens", "Staten Island", "Manhattan"],
+        answer: "Manhattan"
+    },
+    {
+        question_4: "Which county is directly east of Queens?",
+        selection: ["Kings", "Richmond", "Nassau","Sussex"],
+        answer: "Nassau"
+    },
+    {
+        question_5: "What does FDNY stand for?",
+        selection: ["First Dioscese of New York", "First Day in New York", "Fire Department of New York", "Fort Drum New York"],
+        answer: "Fire Department of New York"
+    },
+]
